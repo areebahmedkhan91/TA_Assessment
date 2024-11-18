abb47d5885a1476477a6d583299607cd0e95633d
areeb <areebahmedkhan91@gmail.com>
2024-11-19 02:57:46 +0500

    display conversion component data on history-list component ts file

--- frontend/src/app/components/history-list/history-list.component.ts
+++ frontend/src/app/components/history-list/history-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyApiService } from '../../services/currency-api.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrl: './history-list.component.css'
 })
-export class HistoryListComponent {
+export class HistoryListComponent implements OnInit {
+
+  public history: any[] = [];
+
+  constructor(private apiService: CurrencyApiService) { }
+  ngOnInit(): void {
+    this.apiService.getHistory().subscribe((data) => {
+      this.history = data;
+    })
+  }
 
 }
